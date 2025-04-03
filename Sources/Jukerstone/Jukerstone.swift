64ba3437df0fcd5dde46483a0da950cecc7c9fd7
TSB <tobishittabey@gmail.com>
2025-04-03 23:34:16 +0100

    sdk

--- Sources/Jukerstone/Jukerstone.swift
+++ Sources/Jukerstone/Jukerstone.swift
@@ -1,69 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by TSB M3DIA on 03/04/2025.
+//
+
 import Foundation
-import JukerstoneSDK
-
-public class JukerstoneSDK: ObservableObject {
-    private let playerSDK = JukePodSDK()
-    
-    private let jukerstoneId: String
-    private let developerToken: String
-
-    // Expose values using @Published so SwiftUI can react
-    @Published public private(set) var progress: Double = 0.0
-    @Published public private(set) var currentTime: Double = 0.0
-    @Published public private(set) var duration: Double = 0.0
-
-    public var player: Jukerstone {
-        Jukerstone(
-            sdk: playerSDK,
-            jukerstoneId: jukerstoneId,
-            developerToken: developerToken
-        )
-    }
-    
-    private var updateTimer: Timer?
-
-    public init(jukerstoneId: String, developerToken: String) {
-        self.jukerstoneId = jukerstoneId
-        self.developerToken = developerToken
-        
-        updateTimer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { [weak self] _ in
-            guard let self = self else { return }
-            self.progress = self.progress
-            self.currentTime = self.currentTime
-            self.duration = self.duration
-        }
-
-
-        playerSDK.onVideoEnd = {
-            print("✅ [SDKWrapper] Video ended")
-        }
-
-        playerSDK.onNext = {
-            print("✅ [SDKWrapper] Next triggered")
-        }
-
-        playerSDK.onPrevious = {
-            print("✅ [SDKWrapper] Previous triggered")
-        }
-    }
-
-    public func playNow(isrc: String) {
-        playerSDK.playNow(isrc: isrc)
-    }
-
-    public func seek(to percentage: Double) {
-        playerSDK.seek(percentage: percentage)
-    }
-
-    public func pause() {
-        playerSDK.pause()
-    }
-
-    public func resume() {
-        playerSDK.resume()
-    }
-
-    public func stop() {
-        playerSDK.stop()
-    }
-}
